d936acbc4a8d565eaa5c04c0294dff05b192f03f
Nguyen <dangphucnguyen2006@gmail.com>
2026-06-21 13:32:57 +0700

    Create seed_nhanvien.sql

---
+++ database/seeds/seed_nhanvien.sql
@@ -0,0 +1,16 @@
+USE QuanLyThuVien
+GO
+INSERT INTO NhanVien (MatKhau, HoTen, Email, ChucVu)
+
+VALUES 
+
+('admin123', N'Trần Quản Trị', 'admin@thuvien.com', 'QuanLy'),
+
+('staff123', N'Lê Thị Thủ Thư', 'thuthu1@thuvien.com', 'NhanVien'),
+
+('staff123', N'Phạm Văn Thủ Thư', 'thuthu2@thuvien.com', 'NhanVien'),
+
+('staff123', N'Hoàng Lễ Tân', 'letan@thuvien.com', 'NhanVien'),
+
+('staff123', N'Đinh Hỗ Trợ', 'hotro@thuvien.com', 'NhanVien');
+
